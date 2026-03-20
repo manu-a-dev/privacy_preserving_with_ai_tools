360d9c67629400a0cbe294a291ca08482a470860
PC <pc@TheLaptop>
2026-03-20 00:42:12 -0700

    openai_face_replace.py should now handle 3 and 8 people, openai_face_replace.py also works differently, when called it should be given two file paths, one for the initial image, and one for the destination of the generated image

--- scripts/openai/openai_face_replace.py
+++ scripts/openai/openai_face_replace.py
@@ -1,25 +1,25 @@
 import os
+import argparse
 import base64
-import requests
-from PIL import Image # Run pip install Pillow
 from openai import OpenAI # https://github.com/openai/openai-python/discussions/742
 from dotenv import load_dotenv
 
+parser = argparse.ArgumentParser()
+parser.add_argument('image', help='directory of image')
+parser.add_argument('destination',help ='the filepath for the newly created image')
+args = parser.parse_args()
+
+#sets up openai client
 load_dotenv()
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-client = OpenAI(
-  api_key=os.getenv('OPENAI_API_KEY'),
-)
-# to edit existing image
-def censor(image):
-  text = "remove the face in the picture, replace it with an ai generated face"
-  out = client.images.edit(
-  model ="gpt-image-1.5",
-  image= [open(image,'rb')],
-  prompt=text,
-  n=1)
-  return base64.b64decode(out.data[0].b64_json)
+text = "find all the people in this image, remove their heads, then replace them with new unique heads"
+out = client.images.edit(
+model ="gpt-image-1.5",
+image= [open(args.image,'rb')],
+prompt=text,
+n=1)
+edited = base64.b64decode(out.data[0].b64_json)
+with open(args.destination,'wb') as file:
+  file.write(edited)
 
-edited= censor('./imgs/random_man.jpg')
-with open('./imgs/openai/random_man_openai.png','wb') as file:
-	file.write(edited)
